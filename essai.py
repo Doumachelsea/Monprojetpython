742e56278181e955c1f8c8195f1f9755b42f1c25
Mamadou Marigo <mamadoumarigo1994@gmail.com>
2019-04-09 10:20:24 +0000

    Update essai.py

---
+++ essai.py
@@ -0,0 +1,8 @@
+print("projet : essai sur l'utilisation de python")
+
+X=input("La premiere valeur")
+Y=input("La deuxieme valeur")
+X=int(X)
+Y=int(Y)
+Moyenne=(X+Y)/2
+print("la moyenne de x et y =", Moyenne)
