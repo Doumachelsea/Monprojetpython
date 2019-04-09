570db16dea82d11539e5956ccedbf69610a05962
Mamadou Marigo <mamadoumarigo1994@gmail.com>
2019-04-09 14:36:48 +0000

    my first commit

--- essai.py
+++ essai.py
@@ -1,8 +1,9 @@
-print("projet : essai sur l'utilisation de python")
+#mon projet python
 
 X=input("La premiere valeur")
 Y=input("La deuxieme valeur")
-X=int(X)
-Y=int(Y)
-Moyenne=(X+Y)/2
+
+Moyenne=(int (X)+int(Y))/2
 print("la moyenne de x et y =", Moyenne)
+#deuxiememethode 
+#print(Moyenne)
