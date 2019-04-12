c3ff21373df08c70d3ccddd666d40c56afd5b275
Doumachelsea <49405220+Doumachelsea@users.noreply.github.com>
2019-04-12 09:11:02 +0000

    Add files via upload

---
+++ troisessais.py
@@ -0,0 +1,10 @@
+from random import randint
+x=randint(0,9)
+tentative=3
+while int(tentative) > int(x):
+	y=input("saisir la réponse")
+if int(y)==int(x):
+	print("bravo, vous avez gagné")
+	
+	
+	
